4e45adbaf7ad01d0c48a43295d1027c4e2d7a53b
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2025-06-21 20:35:35 +0800

    added simple call to API

--- src/main/java/net/groundgurus/day3/TodoApp.java
+++ src/main/java/net/groundgurus/day3/TodoApp.java
@@ -1,4 +1,15 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-void main() {
-
+void main() throws Exception {
+    try (var client = HttpClient.newHttpClient()) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/todos/1"))
+                .build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + response.body());
+    }
 }
